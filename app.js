8ff4e49cd3ea00ad2102481ebeead4aadac2a305
t t <tt@tinoMacBook-Air.local>
2022-01-29 10:10:17 +0900

    [Finish]

---
+++ app.js
@@ -0,0 +1,30 @@
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('安い');
+} else if(orange == apple){
+  alert('同じ');
+} else{
+  alert('高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let num = 0;
+
+for (i=1, i<11, i++){
+  num = num + i;
+}
+
+alert('1から10まで' + num + 'だ');
