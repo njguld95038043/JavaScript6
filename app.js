cf68b08df93684d65fcbfb34be9f0d8f78f4e9df
t t <tt@tinoMacBook-Air.local>
2022-01-29 10:31:21 +0900

    [Finish]

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
-let orange = 100;
-let apple = 120;
+let str = "webcamp"
 
-if(orange < apple){
-  alert('安い');
-} else if(orange == apple){
-  alert('同じ');
-} else{
-  alert('高い');
+function foo() { 
+  console.log(str)
+  let str = "dmm webcamp"
+  console.log(str)
 }
 
-let max = 100;
-let num = 1;
-let count = 0;
-
-while(num < max){
-  num = num * 2;
-  count = count + 1
-}
-
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
-
-let i;
-let num = 0;
-
-for (i=1, i<11, i++){
-  num = num + i;
-}
-
-alert('1から10まで' + num + 'だ');
+foo()
